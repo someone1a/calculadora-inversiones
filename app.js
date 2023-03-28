da8e996e652d327a8b5b7276d705683ffda29116
Walter Carrasco <carrascowalter440@gmail.com>
2023-03-28 17:56:11 -0300

    advanced added but no fixed

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 let calculate_money = () => {
     let money = document.getElementById("money").value
-    let plazo_fijo = document.getElementById("plazo").value
-    let calculo = (plazo_fijo/12)*money/100
+    let plazo = document.getElementById("plazo").value
     let app = document.getElementById("app_container")
     let p = document.getElementById("app")
-    p.innerHTML=`El monto que se va a generar en un mes ${calculo}`
+    let calculo = (plazo / 12) * money / 100
+    p.innerHTML = `El monto que se va a generar en un mes ${calculo}`
     app.appendChild(p)
 }
 // CALCULO MEJORADO: plazo/12*plata/100 + plata
+
+let calculate_advanced = () => {
+    let money = document.getElementById("money").value
+    let plazo = document.getElementById("plazo").value
+    let app = document.getElementById("app_container")
+    let p = document.getElementById("app")
+    let time = document.getElementById("time").value
+    let result = 0
+    for (let i = 0; i <= time; i++) {
+        let interes = money * plazo
+        result + interes
+        money + interes
+    }
+    let f = money
+    p.innerHTML=`El dinero generado en ${time} meses es de ${f}`
+    console.log(`El dinero generado en ${time} meses es de ${f}`)
+    app.appendChild(p)
+}
