c5f9de8f2a4624128b0bc334715f2d8f9ec00f67
Walter carrasco <Walter Carrasco>
2024-05-08 09:47:20 -0300

    8-5 changes

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # calculadora-ingreso-deposito
-https://calculadora-plazo-fijo.netlify.app = calculos para un mes <br>
-https://calculadora-plazo-fijo.netlify.app/advanced.html = calculos para una cantidad de meses indicado por el usuario
+https://calculadora-plazo-fijo.netlify.app = calculos para una cantidad de dias
--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 let calculate_money = () => {
-    let money = parseFloat(document.getElementById("money").value)
-    let plazo = parseFloat(document.getElementById("plazo").value)
-    let app = document.getElementById("result")
-    let p = document.getElementById("p")
-
-    // Obtener el último día del mes actual
-    let today = new Date()
-    let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
-
-    let calculo = (plazo / 12 * money / 100 + money)
-    let ingresos_mes = calculo - money
-    let monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-    let nombre_mes = monthNames[today.getMonth()]
-
-    let ingresos_dia = ingresos_mes / lastDayOfMonth
-
-    p.innerHTML = `El monto que va a tener en su cuenta después de un mes es de: ${calculo}. <br> Ingresos obtenidos durante el mes de ${nombre_mes}: ${ingresos_mes.toFixed(2)}. <br> Ingresos generados por día: ${ingresos_dia.toFixed(2)}`  
-    result.appendChild(p)
-}
+    let money = parseFloat(document.getElementById("money").value);
+    let plazo = parseFloat(document.getElementById("plazo").value);
+    let days = parseFloat(document.getElementById("days").value);
+    let app = document.getElementById("result");
+    let p = document.createElement("p");
+    let calculo = (plazo / 12 / 30) * money / 100 + money; // Corrección del cálculo
+    p.innerHTML = `El total generado en ${days} días es ${calculo.toFixed(2)}`; // toFixed(2) para mostrar solo 2 decimales
+    app.appendChild(p);
+};
 
 
 // CALCULO MEJORADO: plazo/12*plata/100 + plata
-
-let calculate_advanced = () => {
-    let money = parseFloat(document.getElementById("money").value)
-    let plazo = parseFloat(document.getElementById("plazo").value)
-    let app = document.getElementById("result")
-    let p = document.getElementById("p")
-    let time = document.getElementById("time").value
-    /*let result = 0
-    for (let i = 0; i <= time; i++) {
-        let interes = money * plazo
-        result + interes
-        money + interes
-    } */
-    let calculo = parseFloat((((plazo / 12) * money / 100) * time) + money)
-    p.innerHTML = `El dinero generado en ${time} meses es de $${calculo}`
-    app.appendChild(p)
-}
